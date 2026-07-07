926ea429384783568de0b5403f10525a3e9046b2
dmz <dm799166@gmail.com>
2026-07-07 14:15:40 -0400

    feat(auth): Implementacion de validador de entradas con Zod:

--- server/api/routes/authRoutes.ts
+++ server/api/routes/authRoutes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { authController } from "../controllers/authController.ts";
+import { validateResource } from "../middlewares/validate.middleware.ts";
+import { loginSchema, registerSchema } from "../schemas/authSchema.ts";
 
 const router = Router();
 
 // Endpoint para iniciar sesión
-router.post("/login", authController.login);
-router.post("/register", authController.register);
+router.post("/login", validateResource(loginSchema), authController.login);
+router.post(
+  "/register",
+  validateResource(registerSchema),
+  authController.register,
+);
 
 export default router;
